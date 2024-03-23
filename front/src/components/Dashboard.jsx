a5257570901c10eba8e26bc3a5ed8d639b3fbf61
Charla19 <charlap20.aps2a@gmail.com>
2024-03-24 01:56:53 +0300

    animation dashboard

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -5,56 +5,56 @@ import "../pages/style.css";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div className="dashboard all-board">
       {/** 1 er niveau */}
-      <div className="flex flex-row justify-between" style={{ flex: 0.5 }}>
+      <div className="flex flex-row justify-between aaa aaa1" style={{ flex: 0.5 }}>
         <div
-          className="flex flex-col h-full bg-green-400 rounded-3xl"
+          className="flex flex-col h-full bg-green-400 rounded-3xl aaa1-1"
           style={{ flex: 0.58 }}
         ></div>
         <div
-          className="flex flex-col h-full bg-sky-400 rounded-3xl"
+          className="flex flex-col h-full bg-sky-400 rounded-3xl aaa1-1"
           style={{ flex: 0.4 }}
         ></div>
       </div>
 
       {/** 2 eme niveau */}
       <div
-        className="flex flex-row justify-between  my-3"
+        className="flex flex-row justify-between  my-3 aaa aaa2"
         style={{ flex: 0.25 }}
       >
         <div
-          className="flex flex-col h-full bg-red-400 rounded-3xl"
+          className="flex flex-col h-full bg-red-400 rounded-3xl aaa2-1"
           style={{ flex: 0.5 }}
         ></div>
         <div
-          className="flex flex-col h-full lg:mx-4 bg-zinc-400 rounded-3xl"
+          className="flex flex-col h-full lg:mx-4 bg-zinc-400 rounded-3xl aaa2-2"
           style={{ flex: 0.5 }}
         >
           
         </div>
         <div
-          className="flex flex-col h-full bg-fuchsia-400 rounded-3xl"
+          className="flex flex-col h-full bg-fuchsia-400 rounded-3xl aaa2-3"
           style={{ flex: 0.5 }}
         ></div>
       </div>
 
       {/** 3 eme niveau */}
-      <div className="flex flex-row " style={{ flex: 0.3 }}>
+      <div className="flex flex-row aaa aaa3" style={{ flex: 0.3 }}>
         <div
-          className="flex flex-col h-full bg-zinc-400 rounded-3xl"
+          className="flex flex-col h-full bg-zinc-400 rounded-3xl aaa3-1"
           style={{ flex: 0.5 }}
         ></div>
         <div
-          className="flex flex-col h-full mx-4 bg-indigo-400 rounded-3xl"
+          className="flex flex-col h-full mx-4 bg-indigo-400 rounded-3xl aaa3-2"
           style={{ flex: 0.5 }}
         ></div>
         <div
-          className="flex flex-col h-full mr-4 bg-orange-400 rounded-3xl"
+          className="flex flex-col h-full mr-4 bg-orange-400 rounded-3xl aaa3-3"
           style={{ flex: 0.5 }}
         ></div>
         <div
-          className="flex flex-col h-full bg-amber-400 rounded-3xl"
+          className="flex flex-col h-full bg-amber-400 rounded-3xl aaa3-4"
           style={{ flex: 0.5 }}
         ></div>
       </div>
