f8c6336bd2895a3a669322da9513471cbdfe9511
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-03-24 10:55:22 +0300

    test

--- backend/home_back/src/weather/weather.service.ts
+++ backend/home_back/src/weather/weather.service.ts
@@ -2,11 +2,11 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { Observable, from, switchMap, tap } from 'rxjs';
 import { WeatherResponseDto } from './dto/weather.dto';
-import { AxiosResponse } from 'axios';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HomeStore } from 'src/entities/HomeStore';
 import { Repository } from 'typeorm';
 import { WeatherStore } from 'src/entities/WeatherStore';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class WeatherService {
@@ -19,27 +19,32 @@ export class WeatherService {
         private weatherStoreRepository: Repository<WeatherStore>
     ) {}
 
-    findCurrentWeather(home_store_id: string): Observable<AxiosResponse<WeatherResponseDto>> {
-        return from(this.homeStoreRepository.findOneBy({ id: home_store_id })).pipe(
-            switchMap(
-                (result) => this.httpService.get(
-                    `${process.env["WEATHER_API"]}/forecast?latitude=${result.latitude}&longitude=${result.longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&forecast_days=1`
-                )
-            ),
-            tap(response => this.persistWeather(response.data as WeatherResponseDto))
-        );
+    async findCurrentWeather(home_store_id: string): Promise<WeatherResponseDto> {
+        const result = await this.homeStoreRepository
+            .createQueryBuilder('h')
+            .select([
+                'h.latitude as latitude', 'h.longitude as longitude'
+            ])
+            .where(`h.id=:identifiant`, {identifiant: home_store_id})
+            .getRawOne();
+        const response = await axios.get(`${process.env["WEATHER_API"]}/forecast?latitude=${result.latitude}&longitude=${result.longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&forecast_days=1`);
+        return response.data as WeatherResponseDto;
+        // await this.persistWeather(data);
+        // return data;
     }
 
-    private persistWeather(data: WeatherResponseDto): void {
-        this.weatherStoreRepository.createQueryBuilder()
-        .insert()
-        .into(WeatherStore)
-        .values({
-            temperature: data.current.temperature_2m,
-            precipitation:  data.current.precipitation,
-            rain: data.current.rain,
-            windSpeed: data.current.wind_speed_10m,
-            humidity: data.current.relative_humidity_2m
-        }).execute();
-    }
+    // private async persistWeather(data: WeatherResponseDto): Promise<void> {
+    //     console.log(data);
+        
+    //     await this.weatherStoreRepository.createQueryBuilder()
+    //     .insert()
+    //     .into(WeatherStore)
+    //     .values({
+    //         temperature: data.current.temperature_2m,
+    //         precipitation:  data.current.precipitation,
+    //         rain: data.current.rain,
+    //         windSpeed: data.current.wind_speed_10m,
+    //         humidity: data.current.relative_humidity_2m
+    //     }).execute();
+    // }
 }
---
+++ backend/local/postgres/init1.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('user', 'user', 'lahatra.ravelonarivo@gmail.com', 'user', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Lauretta', 'Hensmans', 'bhensmans1@utexas.edu', 'Bard', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Mariam', 'Reid', 'freid2@blogspot.com', 'Franklin', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Axe', 'Millican', 'imillican3@blogger.com', 'Ilsa', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Charley', 'Gowdy', 'sgowdy4@unc.edu', 'Stepha', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Katie', 'Barthrup', 'ebarthrup5@istockphoto.com', 'Elysee', SHA512('home'));
+INSERT INTO public.user_store (fist_name, last_name, email, pseudo, password) VALUES ('Penn', 'Skitral', 'bskitral6@columbia.edu', 'Barby', SHA512('home'));
+
+
+INSERT INTO public.home_store (label, latitude, longitude, uptime, status, current_energy_consumption) VALUES ('Thoughtbridge', 53.4413995, 19.0963213, 5, false, 89);
+INSERT INTO public.home_store (label, latitude, longitude, uptime, status, current_energy_consumption) VALUES ('Trilia', 38.0329785, 23.831904, 42, false, 38);
+INSERT INTO public.home_store (label, latitude, longitude, uptime, status, current_energy_consumption) VALUES ('Yakijo', 50.3713333, 103.2816977, 75, false, 95);
